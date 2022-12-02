5687b04648e74dd5888c76262dd85aac8a75eca8
lryjacobs1 <ljacobs@mxtrautomation.com>
2022-12-02 13:44:05 -0600

    Running Build

---
+++ docs/assets/index.html.a907dac1.js
@@ -0,0 +1,27 @@
+import{_ as r,r as d,o,c,a as e,b as t,t as u,d as n,w as i,e as m}from"./app.96878e34.js";const h={},v={id:"page-title",tabindex:"-1"},b=e("a",{class:"header-anchor",href:"#page-title","aria-hidden":"true"},"#",-1),p={href:"https://www.mxtrautomation.com/",target:"_blank",rel:"noopener noreferrer"},f=m(`<h2 id="technical-stack" tabindex="-1"><a class="header-anchor" href="#technical-stack" aria-hidden="true">#</a> Technical stack</h2><table><thead><tr><th>Technology</th><th>Description</th></tr></thead><tbody><tr><td>JavaScript/TypeScript</td><td>Language</td></tr><tr><td>Vue.js (2.6.x)</td><td>Core framework</td></tr><tr><td>Jest</td><td>Testing framework</td></tr><tr><td>Eslint</td><td>Code checking</td></tr><tr><td>Vuetify</td><td>Ui components framework</td></tr><tr><td>Vuex</td><td>Reactive state management library</td></tr><tr><td>Vue Router</td><td>Page router</td></tr><tr><td>Scss/Sass/Stylus</td><td>Css preprocessors</td></tr></tbody></table><h2 id="file-system" tabindex="-1"><a class="header-anchor" href="#file-system" aria-hidden="true">#</a> File System</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>└─ src
+   ├─ api
+   │  └─ loginAPI.js
+   ├─ assets
+   ├─ classes
+   ├─ common
+   ├─ components
+   ├─ constants
+   ├─ development
+   ├─ helpers
+   ├─ layouts
+   ├─ mixins
+   ├─ plugins
+   ├─ sass
+   ├─ store
+   │  ├─ modules
+   │  └─ index.js
+   ├─ views
+   │  ├─ admin
+   │  ├─ dashboard
+   │  ├─ workflow
+   │  └─ ...
+   ├─ App.vue
+   ├─ globalComponents.js
+   ├─ main.ts
+   └─ router.js
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="docs-contents" tabindex="-1"><a class="header-anchor" href="#docs-contents" aria-hidden="true">#</a> Docs contents</h2>`,5),_=e("a",{href:"./workflows"},"Workflow",-1),w=e("li",null,[e("a",{href:"./email-editor"},"Email Editor")],-1),k=e("li",null,[e("a",{href:"./lead-lists"},"Lead Lists")],-1);function x(a,g){const l=d("ExternalLinkIcon"),s=d("RouterLink");return o(),c("div",null,[e("h1",v,[b,t(" "+u(a.$page.title),1)]),e("p",null,[t("This documentation is created in order to support, at first place, Front-end developers to understand the UI system of the "),e("a",p,[t('"MXTR Core user dashboard"'),n(l)]),t(" project. Here you can find description of the core components, code examples and important logic peaces explanation.")]),f,e("ul",null,[e("li",null,[_,e("ul",null,[e("li",null,[n(s,{to:"/workflows/sections.html"},{default:i(()=>[t("Activities (Sections)")]),_:1})]),e("li",null,[n(s,{to:"/workflows/connections.html"},{default:i(()=>[t("Connections")]),_:1})]),e("li",null,[n(s,{to:"/workflows/sectionModals.html"},{default:i(()=>[t("Section Modals")]),_:1})]),e("li",null,[n(s,{to:"/workflows/sectionMenu.html"},{default:i(()=>[t("Section Menu")]),_:1})]),e("li",null,[n(s,{to:"/workflows/technical.html"},{default:i(()=>[t("Workflow under the hood")]),_:1})])])]),w,k])])}const S=r(h,[["render",x],["__file","index.html.vue"]]);export{S as default};
